8bdf73e8d11576dafec80c2478b379e2d86ef398
Sruthin31 <153161309+Sruthin31@users.noreply.github.com>
2026-04-02 08:10:50 +0000

    functional component: show paragraph on button click using useState

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,16 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [count,setCount] =useState(false);
+  const handleClick = ()=>{
+    setCount(true);
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      
+      <button id="click" onClick={handleClick}>Click Me</button>
+      {count &&(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)}
+
     </div>
   );
 }
